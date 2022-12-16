40222a22de12e2e7878ffb68005801a69bba14e1
Conejo Acosta <alberto.conejoacosta@abbott.com>
2022-12-16 16:07:54 -0600

    cambios

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,14 +1,15 @@
-import React from 'react';
+import React from "react";
 
-import { Content } from '../content/Content';
-import { Meta } from '../layout/Meta';
-import { Main } from '../templates/Main';
+import { Content } from "../content/Content";
+import { Meta } from "../layout/Meta";
+import { Main } from "../templates/Main";
 
 const About = () => (
   <Main meta={<Meta title="Lorem ipsum" description="Lorem ipsum" />}>
     <Content>
       <p>
-        Software engineering graduate from the Universidad Nacional de Costa Rica
+        Software engineering graduate from the Universidad Nacional de Costa
+        Rica.
       </p>
     </Content>
   </Main>
