7d8fc734729ace2cddc32f0db418037ee13e7b90
Conejo Acosta <alberto.conejoacosta@abbott.com>
2022-12-16 15:21:11 -0600

    cambioss

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,8 +1,7 @@
 export const AppConfig = {
   site_name: 'Starter',
   title: 'Alberto Conejo',
-  description:
-    'Si lo puedes imaginar, lo puedes programar',
+  description:'Si lo puedes imaginar, lo puedes programar',
   url: 'https://example.com',
   locale: 'en',
   author: 'Anonymous',
